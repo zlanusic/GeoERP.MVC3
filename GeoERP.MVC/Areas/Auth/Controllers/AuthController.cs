08a56882ad7e15d74f8b0b004c4f90b94f9fe2a5
zlanusic <zlatko.anusic@zlanusic.com>
2013-08-24 20:59:10 +0200

    Added new areas files and some library references

---
+++ GeoERP.MVC/Areas/Auth/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeoERP.MVC.Areas.Auth.Models;
+
+namespace GeoERP.MVC.Areas.Auth.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/Auth/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Potrebno za swapping _login view(formu)
+        /// </summary>
+        /// <returns>Parcijalni view _Login</returns>
+        public PartialViewResult _Login()
+        {
+            return PartialView();
+        }
+
+        /// <summary>
+        /// Prijavljuje korisnika.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult _Login(LoginModel model)
+        {
+            RedirectToAction("AdminCompany", "Admin");
+            return View();
+        }
+
+    }
+}
